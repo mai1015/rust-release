ca27339e14f66e24619e25d86d612c070cadca59
mai1015 <i@mai1015.com>
2024-05-10 15:44:42 -0400

    feat: process hash later

--- src/fs.rs
+++ src/fs.rs
@@ -4,7 +4,10 @@ use tokio::{io};
 #[cfg(feature = "async")]
 use async_recursion::async_recursion;
 
-// use tokio::sync::mpsc::Sender;
+#[cfg(feature = "async")]
+use tokio::sync::mpsc;
+#[cfg(feature = "async")]
+use tokio::sync::mpsc::Sender;
 
 #[cfg(not(feature = "async"))]
 use std::{io};
@@ -14,6 +17,7 @@ use std::path::Path;
 use std::sync::Arc;
 use std::sync::atomic::AtomicUsize;
 use std::time::SystemTime;
+use log::log;
 use crate::data::FileData;
 use crate::hash::calculate_file_hash;
 use crate::node::dir::DirectoryNode;
@@ -29,29 +33,50 @@ use crate::node::Node;
 //     Io(#[from] std::io::Error),
 // }
 
+// #[cfg(feature = "async")]
+// #[derive(Debug)]
+// struct HashData {
+//     path: String,
+//     hash: String,
+// }
 #[cfg(feature = "async")]
-#[derive(Debug)]
-struct HashData {
-    path: String,
-    hash: String,
+#[derive(Debug, Clone)]
+pub struct ProgressData {
+    total: usize,
+    completed: usize,
 }
 
 #[cfg(feature = "async")]
-pub async fn generate_file_data_from_path<P: AsRef<Path>>(path: P, ignore: &Vec<String>) -> io::Result<FileData> {
-    let counter = Arc::new(AtomicUsize::new(0));
-    // let (tx, mut rx) = mpsc::channel(64);
+struct SafeFileNodePtr(*mut FileNode);
+
+#[cfg(feature = "async")]
+unsafe impl Send for SafeFileNodePtr {}
+
+#[cfg(feature = "async")]
+pub async fn generate_file_data_from_path_with_progress<P: AsRef<Path>>(path: P, ignore: &Vec<String>, ptx: Sender<ProgressData>) -> io::Result<FileData> {
+    let mut progress = ProgressData {
+        total: 0,
+        completed: 0,
+    };
+    let total_counter = Arc::new(AtomicUsize::new(0));
+    let done_counter = Arc::new(AtomicUsize::new(0));
+    
     // calculate time running 
     let start = SystemTime::now();
-    let root = generate_root_from_path(path.as_ref(), "", ignore, counter.clone()).await?;
+    let root = generate_root_from_path(path.as_ref(), "", ignore, total_counter.clone()).await?;
     log::info!("Scan Done Elapsed time: {}s", SystemTime::now().duration_since(start).unwrap().as_secs());
     loop {
-        if counter.load(std::sync::atomic::Ordering::SeqCst) == 0 {
+        progress.total = total_counter.load(std::sync::atomic::Ordering::SeqCst);
+        progress.completed = done_counter.load(std::sync::atomic::Ordering::SeqCst);
+        if progress.total <= progress.completed {
             break;
         }
+        
+        ptx.send(progress.clone()).await.unwrap();
         tokio::time::sleep(std::time::Duration::from_millis(10)).await;
     }
     log::info!("Async Elapsed time: {}s", SystemTime::now().duration_since(start).unwrap().as_secs());
-    
+
     let data = FileData::new(
         path.as_ref().to_str().unwrap().to_string(),
         0,
@@ -60,6 +85,68 @@ pub async fn generate_file_data_from_path<P: AsRef<Path>>(path: P, ignore: &Vec<
     return Ok(data);
 }
 
+#[cfg(feature = "async")]
+pub async fn generate_file_data_from_path<P: AsRef<Path>>(path: P, ignore: &Vec<String>) -> io::Result<FileData> {
+    let mut progress = ProgressData {
+        total: 0,
+        completed: 0,
+    };
+    let total_counter = Arc::new(AtomicUsize::new(0));
+    let done_counter = Arc::new(AtomicUsize::new(0));
+
+    // calculate time running 
+    let start = SystemTime::now();
+    let mut root = generate_root_from_path(path.as_ref(), "", ignore, total_counter.clone()).await?;
+    log::info!("Scan Done Elapsed time: {}s", SystemTime::now().duration_since(start).unwrap().as_secs());
+    generate_file_hash_for_node(&mut root, done_counter.clone()).await;
+    // let root = 
+    loop {
+        progress.total = total_counter.load(std::sync::atomic::Ordering::SeqCst);
+        progress.completed = done_counter.load(std::sync::atomic::Ordering::SeqCst);
+        if progress.total <= progress.completed {
+            break;
+        }
+
+        log::info!("Progress: {}/{}", progress.completed, progress.total);
+        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    }
+    log::info!("Async Elapsed time: {}s", SystemTime::now().duration_since(start).unwrap().as_secs());
+
+    let data = FileData::new(
+        path.as_ref().to_str().unwrap().to_string(),
+        0,
+        root,
+    );
+    return Ok(data);
+}
+
+#[cfg(feature = "async")]
+#[async_recursion]
+pub async fn generate_file_hash_for_node(node: &mut DirectoryNode, done: Arc<AtomicUsize>) {
+    for child in node.children.iter_mut() {
+        match child {
+            Node::File(file) => {
+                let ptr = SafeFileNodePtr(file as *mut FileNode);
+                let path = file.get_path();
+                let counter_clone = done.clone();
+                tokio::spawn(async move {
+                    let hash = calculate_file_hash(&path).await.unwrap();
+                    let p = ptr;
+                    // log::debug!("Calculated hash for: {}", path.to_str().unwrap_or_default());
+                    // tokio::time::sleep(std::time::Duration::from_millis(10));
+                    unsafe {
+                        (*p.0).set_hash(hash);
+                    }
+                    // log::debug!("Setting hash for: {}", path.to_str().unwrap_or_default());
+                    counter_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                });
+            },
+            Node::Directory(dir) => {
+                generate_file_hash_for_node(dir, done.clone()).await;
+            }
+        }
+    }
+}
 
 #[cfg(not(feature = "async"))]
 pub fn generate_file_data_from_path<P: AsRef<Path>>(path: P, ignore: &Vec<String>) -> io::Result<FileData> {
@@ -85,15 +172,8 @@ fn join_path(path: &str, name: &str) -> String {
 }
 
 #[cfg(feature = "async")]
-struct SafeFileNodePtr(*mut FileNode);
-
-#[cfg(feature = "async")]
-unsafe impl Send for SafeFileNodePtr {}
-
-//, tx: Sender<HashData>
-#[cfg(feature = "async")]
 #[async_recursion]
-pub async fn generate_root_from_path<P: AsRef<Path> + std::marker::Send>(path: P, relative_path: &str, ignore: &Vec<String>, counter: Arc<AtomicUsize>) -> io::Result<DirectoryNode> {
+pub async fn generate_root_from_path<P: AsRef<Path> + std::marker::Send>(path: P, relative_path: &str, ignore: &Vec<String>, total: Arc<AtomicUsize>) -> io::Result<DirectoryNode> {
     log::debug!("Generate from: {}", path.as_ref().to_str().unwrap_or_default());
     let mut data = if relative_path == "" {
         DirectoryNode::new(
@@ -126,7 +206,7 @@ pub async fn generate_root_from_path<P: AsRef<Path> + std::marker::Send>(path: P
             if should_ignore {
                 continue;
             }
-            data.add_child(Node::Directory(generate_root_from_path(path_str, &rp, ignore, counter.clone()).await?));
+            data.add_child(Node::Directory(generate_root_from_path(path_str, &rp, ignore, total.clone()).await?));
         } else {
             let metadata = fs::metadata(&path)?;
             let last_modified = metadata.modified()?.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
@@ -136,21 +216,7 @@ pub async fn generate_root_from_path<P: AsRef<Path> + std::marker::Send>(path: P
                 last_modified,
             );
 
-            let counter_clone = counter.clone();
-            counter_clone.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-
-            let ptr = SafeFileNodePtr(&mut file_data as *mut FileNode);
-            tokio::spawn(async move {
-                let hash = calculate_file_hash(&path).await.unwrap();
-                let p = ptr;
-                // log::debug!("Calculated hash for: {}", path.to_str().unwrap_or_default());
-                // tokio::time::sleep(std::time::Duration::from_millis(10));
-                unsafe {
-                    (*p.0).set_hash(hash);
-                }
-                log::debug!("Setting hash for: {}", path.to_str().unwrap_or_default());
-                counter_clone.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
-            });
+            total.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
             data.add_child(Node::File(file_data));
         }
     }
